3df0685ed35dbe87660b8f3fed04638577a80e48
Viresh Tarapur <tarapurviresh@gmail.com>
2026-03-06 15:02:12 +0530

    Obscure MySQL password and database name
    Updated MySQL configuration to obscure sensitive information.

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ app.secret_key = "secret_key"
 # MYSQL CONFIG
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root123'
-app.config['MYSQL_DB'] = 'mydatabase'
+app.config['MYSQL_PASSWORD'] = '--------'
+app.config['MYSQL_DB'] = '----'
 
 mysql = MySQL(app)
 
