a9c4992af1fa27b746d3a84a22f7ae3f3b5cff60
Viresh Tarapur <tarapurviresh@gmail.com>
2026-03-06 15:02:36 +0530

    Change MySQL credentials to placeholders
    Updated MySQL configuration to use placeholder values.

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root123'
-app.config['MYSQL_DB'] = 'mydatabase'
+app.config['MYSQL_PASSWORD'] = '----'
+app.config['MYSQL_DB'] = '----'
 
 mysql = MySQL(app)
 
